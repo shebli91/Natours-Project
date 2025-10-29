4d5db5e97dd3085a9d256ae0130fe9f9c68164c0
Shebli91 <mhamadshebli@gmail.com>
2025-10-29 19:55:25 +0200

    - Implementing the POST logic - Implementing GET tour by ID logic

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { resourceLimits } = require('worker_threads');
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -14,6 +15,42 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length,
     data: { tours },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(res.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
+    }
+  );
 });
 
 const port = 3000;
