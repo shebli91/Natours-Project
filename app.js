9c936a64966e004be28feb8d8c082b95e1421963
Shebli91 <mhamadshebli@gmail.com>
2025-10-30 00:20:34 +0200

    creating a custom middleware and use next() - implementing the requested time middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hellow from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -11,6 +21,7 @@ const tours = JSON.parse(
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: { tours },
   });
