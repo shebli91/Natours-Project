1ddd4590147cf293f01ce56a17eb87a808e514da
Shebli91 <mhamadshebli@gmail.com>
2025-11-01 17:43:04 +0200

    - implementing the MongoDB configurations - Connecting to the DB cluster - refactoring the create tour - refactoring the  get all tours method

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB Connection Successful!');
+  });
 
 // console.log(process.env);
 
