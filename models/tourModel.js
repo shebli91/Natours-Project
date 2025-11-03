10ec9ff29fe2f6747c07b2405514fe6314f46fa3
Shebli91 <mhamadshebli@gmail.com>
2025-11-03 18:48:46 +0200

    building a custom validator to check for if the discount price is below the regular price

--- models/tourModel.js
+++ models/tourModel.js
@@ -46,7 +46,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only point to current doc on NEW document creation
+          return val < this.price; // 100 < 200
+        },
+        message: 'Discount Price ({VALUE}) should be below the regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
