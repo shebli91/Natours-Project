44fa89decfffa0b843f1dd2c8127b8342f2e1136
Shebli91 <mhamadshebli@gmail.com>
2025-11-04 02:03:00 +0200

    -Implementing of handling the duplicated fields and the Invalid IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,58 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicatedFieldsDB = (err) => {
+  const value = Object.values(err.keyValue)[0];
+  const message = `Duplicated field value: [${value}]. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const sendErrorForDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorForProd = (err, res) => {
+  // operational, trusted error: send to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error: don't leak  error details
+  } else {
+    // 1) Log error
+    console.log('ERROR 💥', err);
+
+    // 2) Send a generic message
+    res.status(500).json({
+      status: 'error',
+      message: ' Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorForDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    error.message = err.message;
+    error.name = err.name;
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicatedFieldsDB(error);
+    sendErrorForProd(error, res);
+  }
 };
