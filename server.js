cc689682bd27691272975969fdbffaaca2817cea
Shebli91 <mhamadshebli@gmail.com>
2025-11-04 02:32:45 +0200

    Implementing the error handler for Unhandled error rejections

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ mongoose
 // console.log(process.env);
 
 const port = process.env.port || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLER REJECTION! 💥 Shuting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
