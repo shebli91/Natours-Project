84099d38b60802d28e3b2b6604317b39befc9f35
Shebli91 <mhamadshebli@gmail.com>
2025-11-04 02:23:13 +0200

    Implementing the handelValidationErrorDB

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -11,6 +11,12 @@ const handleDuplicatedFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handelValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorForDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -53,6 +59,8 @@ module.exports = (err, req, res, next) => {
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicatedFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handelValidationErrorDB(error);
     sendErrorForProd(error, res);
   }
 };
